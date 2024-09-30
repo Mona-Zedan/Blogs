b67ba34d3b67873d185b0720077b99cb437e1f0a
unknown <monazedan055@gmail.com>
2024-09-30 09:43:48 +0300

    just edite

--- myapp/src/components/sidebar/sidebar.jsx
+++ myapp/src/components/sidebar/sidebar.jsx
@@ -1,38 +1,37 @@
 import "./sidebar.css"
 export default function Sidebar() {
-    return (
-      <div className="sidebar">
-        <div className="sidebarItem">
-            <span className="sidebarItem">ABOUT ME</span>
-            <img
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
           src="https://ultahost.com/blog/wp-content/uploads/2023/01/Photo-Background-Removal-StickerMuleTracer.png"
           alt=""
         />
         <p>
-          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
-          amet ex esse.Sunt eu ut nostrud id quis proident.
+          "I'm David Johnson, and I create and manage a blog <br /> or website that features articles, text content, or media that is regularly updated. <br /> I share my thoughts, experiences, and various topics with the audience."
         </p>
       </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORITES</span>
         <ul className="sidebarList">
-            <li className="sidebarListItem">Life</li>
-            <li className="sidebarListItem">Music</li>
-            <li className="sidebarListItem">Style</li>
-            <li className="sidebarListItem">Sport</li>
-            <li className="sidebarListItem">Tech</li>
-            <li className="sidebarListItem">Cinema</li>
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
         </ul>
       </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">Follow Us</span>
         <div className="sidebarSocial">
-       <i className="sidebarIcon fa-brands fa-twitter"></i>
-       <i className="sidebarIcon fa-brands fa-pinterest-p"></i>
-       <i className="sidebarIcon fa-brands fa-facebook"></i>
-       <i className="sidebarIcon fa-brands fa-instagram"></i>
+          <i className="sidebarIcon fa-brands fa-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-pinterest-p"></i>
+          <i className="sidebarIcon fa-brands fa-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-instagram"></i>
         </div>
       </div>
-      </div>
-    )
-  }
+    </div>
+  )
+}
